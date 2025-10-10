e7a8560884ff45cccf010883a0ce6a8f4254fcc2
ArturMuniz <artur.santos@estudante.ifto.edu.br>
2025-10-10 15:46:13 -0300

    mudança, de nome_usuario  pra matricula

--- cadastro.php
+++ cadastro.php
@@ -7,7 +7,8 @@ include "conexao.php";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap" rel="stylesheet">
@@ -19,7 +20,7 @@ include "conexao.php";
             <form action="./novousuario.php" method="post">
                 <h1>Cadastro</h1>
                 <input id="nome_completo" name="nome_completo" type="text" placeholder="Nome completo" required>
-                <input id="nome_usuario" name="nome_usuario" type="text" placeholder="Nome do usuario" required>
+                <input id="matricula" name="matricula" type="text" placeholder="Matrícula" required>
                 <input id="email" name="email" type="email" placeholder="Email" required>
                 <input id="senha" name="senha" type="password" placeholder="Senha" required>
                 <!--<input id="confirmarsenha" name="confirmarsenha" type="password" placeholder="Confirmar Senha" required>-->
